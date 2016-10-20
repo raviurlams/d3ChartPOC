cfdc2e1ddaa771004caccc6bcc55b406c6643fba
ravindra <rurlam@cisco.com>
2016-10-20 11:55:06 +0530

    first commit

--- app/chartCtrl.js
+++ app/chartCtrl.js
@@ -1,4 +1,4 @@
-// ************** Generate the tree diagram	 *****************
+// ************** Generate the diagram  *****************
 var margin = { top: 20, right: 120, bottom: 20, left: 120 },
     width = 960 - margin.right - margin.left,
     padding = 100,
@@ -7,9 +7,6 @@ var margin = { top: 20, right: 120, bottom: 20, left: 120 },
 var i = 0;
 var max_n = 0;
 var min_n = 0;
-
-var tree = d3.layout.tree()
-    .size([height, width]);
 
 var diagonal = d3.svg.diagonal()
     .projection(function(d) {
@@ -33,6 +30,7 @@ d3.csv("data/treeData.csv", function(error, treeData) {
 function updateProperties(data) {
     max_n = d3.max(data, getYears);
     min_n = d3.min(data, getYears);
+    console.log(min_n,'--',max_n)
     drawYAxis();
     drawChartArea();
 }
@@ -40,12 +38,12 @@ function updateProperties(data) {
 function drawYAxis() {
     // define the y scale  (vertical)
     var yScale = d3.scale.linear()
-        .domain([min_n, max_n]) // values between min_n and max_n
+        .domain([max_n, min_n]) // values between min_n and max_n
         .range([height, 0]);
     // define the y axis
     var yAxis = d3.svg.axis()
         .orient("left")
-        .scale(yScale).ticks(10).tickPadding(8);
+        .scale(yScale).ticks(10).tickFormat(d3.format("04d")).tickPadding(8);
     // draw y axis with labels and move in from the size by the amount of padding
     svg.append("g")
         .attr('class', 'y axis')
@@ -53,12 +51,13 @@ function drawYAxis() {
         .call(yAxis)
         .append("text")
         .attr("transform", "rotate(-90)")
-        .attr("y", 5)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
+        .attr("y", 0 - margin.left+margin.top)
+        .attr("x",0 - (height / 2))
+        .attr("dy", "1em")
+        .style("text-anchor", "middle")       
         .text("Year");
 }
 
-function drawChartArea(){
-	
+function drawChartArea() {
+
 }
