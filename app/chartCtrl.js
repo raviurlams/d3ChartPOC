b781118c036850990d513a529456db7dbb3f4024
ravindra <rurlam@cisco.com>
2016-10-20 12:18:05 +0530

    first commit

--- app/chartCtrl.js
+++ app/chartCtrl.js
@@ -1,36 +1,29 @@
-// ************** Generate the diagram  *****************
+//************* CONSTANTS ***************//
 var margin = { top: 20, right: 120, bottom: 20, left: 120 },
     width = 960 - margin.right - margin.left,
     padding = 100,
-    height = 500 - margin.top - margin.bottom;
+    height = 500 - margin.top - margin.bottom,
+    yaxis_Max = 0,
+    yaxis_Min = 0;
 
-var i = 0;
-var max_n = 0;
-var min_n = 0;
+var YAXIS_LABEL_TEXT = "Year";
+var YAXIS_DATASOURCE_LABEL = "FIRST APPEARED";
+var FILE_PATH = "data/treeData.csv";
 
-var diagonal = d3.svg.diagonal()
-    .projection(function(d) {
-        return [d.y, d.x];
-    });
+//************* END of CONSTANTS ***************//
 
-var svg = d3.select("body").append("svg")
+var svg = d3.select("#chart").append("svg")
     .attr("width", width + margin.right + margin.left)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var getYears = function(d) {
-        return d['FIRST APPEARED']
-    }
-    // load the external data
-d3.csv("data/treeData.csv", function(error, treeData) {
-    updateProperties(treeData);
-});
 
-function updateProperties(data) {
-    max_n = d3.max(data, getYears);
-    min_n = d3.min(data, getYears);
-    console.log(min_n,'--',max_n)
+
+function drawChart(data) {
+    yaxis_Max = d3.max(data, getYears);
+    yaxis_Min = d3.min(data, getYears);
+    
     drawYAxis();
     drawChartArea();
 }
@@ -38,7 +31,7 @@ function updateProperties(data) {
 function drawYAxis() {
     // define the y scale  (vertical)
     var yScale = d3.scale.linear()
-        .domain([max_n, min_n]) // values between min_n and max_n
+        .domain([yaxis_Max, yaxis_Min]) // values between yaxis_Min and yaxis_Max
         .range([height, 0]);
     // define the y axis
     var yAxis = d3.svg.axis()
@@ -46,18 +39,103 @@ function drawYAxis() {
         .scale(yScale).ticks(10).tickFormat(d3.format("04d")).tickPadding(8);
     // draw y axis with labels and move in from the size by the amount of padding
     svg.append("g")
-        .attr('class', 'y axis')
+        .attr('class', 'y axis')        
         .attr("transform", "translate(" + padding + ",0)")
         .call(yAxis)
         .append("text")
         .attr("transform", "rotate(-90)")
-        .attr("y", 0 - margin.left+margin.top)
-        .attr("x",0 - (height / 2))
+        .attr("y", 0 - margin.left + margin.top)
+        .attr("x", 0 - (height / 2))
         .attr("dy", "1em")
-        .style("text-anchor", "middle")       
-        .text("Year");
+        .style("text-anchor", "middle")
+        .text(YAXIS_LABEL_TEXT);
 }
 
 function drawChartArea() {
+    var force = d3.layout.force()
+    .nodes(d3.values(nodes))
+    .links(links)
+    .size([width, height])
+    .linkDistance(60)
+    .charge(-300)
+    .on("tick", tick)
+    .start();
 
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+// build the arrow.
+svg.append("svg:defs").selectAll("marker")
+    .data(["end"])      // Different link/path types can be defined here
+  .enter().append("svg:marker")    // This section adds in the arrows
+    .attr("id", String)
+    .attr("viewBox", "0 -5 10 10")
+    .attr("refX", 15)
+    .attr("refY", -1.5)
+    .attr("markerWidth", 6)
+    .attr("markerHeight", 6)
+    .attr("orient", "auto")
+  .append("svg:path")
+    .attr("d", "M0,-5L10,0L0,5");
+
+// add the links and the arrows
+var path = svg.append("svg:g").selectAll("path")
+    .data(force.links())
+  .enter().append("svg:path")
+//    .attr("class", function(d) { return "link " + d.type; })
+    .attr("class", "link")
+    .attr("marker-end", "url(#end)");
+
+// define the nodes
+var node = svg.selectAll(".node")
+    .data(force.nodes())
+  .enter().append("g")
+    .attr("class", "node")
+    .call(force.drag);
+
+// add the nodes
+node.append("circle")
+    .attr("r", 5);
+
+// add the text 
+node.append("text")
+    .attr("x", 12)
+    .attr("dy", ".35em")
+    .text(function(d) { return d.name; });
+
+// add the curvy lines
+function tick() {
+    path.attr("d", function(d) {
+        var dx = d.target.x - d.source.x,
+            dy = d.target.y - d.source.y,
+            dr = Math.sqrt(dx * dx + dy * dy);
+        return "M" + 
+            d.source.x + "," + 
+            d.source.y + "A" + 
+            dr + "," + dr + " 0 0,1 " + 
+            d.target.x + "," + 
+            d.target.y;
+    });
+
+    node
+        .attr("transform", function(d) { 
+        return "translate(" + d.x + "," + d.y + ")"; });
 }
+
+});
+}
+//************* Helper Functions ***************//
+var getYears = function(d) {
+    return d[YAXIS_DATASOURCE_LABEL]
+};
+
+function refreshChartData() {
+    d3.csv(FILE_PATH, function(error, treeData) {
+        drawChart(treeData);
+    });
+}
+//************* End Helper Functions ***********//
+
+
+refreshChartData();
